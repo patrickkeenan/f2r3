d17b58b3965409b15a85f9b1399c5182590fad26
Patrick Keenan <me@patrickkeenan.me>
2024-03-04 23:41:15 -0500

    fixed next build issue

--- app/layout/route.ts
+++ app/layout/route.ts
@@ -1,10 +1,5 @@
 import path from "path";
 import { writeFile, mkdir } from "fs/promises";
-import { figmaLayoutString } from "../../../components/figma-utils-string";
-import { figmaToComponents } from "../../../components/utils/figma-utils";
-
-import { collection, doc, Timestamp, setDoc } from "firebase/firestore";
-import { db } from "@/lib/firebase/firebase";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 
@@ -21,60 +16,19 @@ export async function GET(request: Request) {
 
 export async function POST(req: Request) {
   const data = await req.json();
-  // const pageId = data.pageId;
-  // const documentId = data.documentId;
-  const documentId = data.id;
-  // const rootLayerName = data.name;
-
-  const isDevelopment = true;
-  if (isDevelopment) {
-    // This is a dev server: Write the files directly to the server
-    let json = JSON.stringify(data, null, 2);
-    try {
-      await figmaToComponents(data);
-      console.log("writing doc", documentId);
-
-      let dirPath = `public/uploads/layouts/${toSafeString(documentId)}`;
-      await mkdir(dirPath, { recursive: true });
-
-      await writeFile(
-        path.resolve(process.cwd(), dirPath, `layout.json`),
-        json
-      );
-
-      // await writeFile(
-      //   path.resolve(process.cwd(), "prototypes", `figma.tsx`),
-      //   figmaLayoutString(data)
-      // );
-
-      console.log("wrote JSON");
-      return new Response("JSON file written successfully", {
-        status: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        },
-      });
-    } catch (err) {
-      console.error(err);
-      return new Response("An error occurred", {
-        status: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        },
-      });
-    }
-  } else {
-    // This is production: Write the files to firebase storage
-    const docRef = doc(collection(db, `prototypes`), toSafeString(documentId));
-    setDoc(docRef, {
-      timestamp: Timestamp.fromDate(new Date()),
-      ...data,
+  try {
+    console.log("wrote JSON");
+    return new Response("JSON file written successfully", {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
     });
-    return new Response("Layout written to database successfully", {
+  } catch (err) {
+    console.error(err);
+    return new Response("An error occurred", {
       status: 200,
       headers: {
         "Access-Control-Allow-Origin": "*",
