dab6c8d35bbdb216ddf2491ee5292edcbf37f694
Patrick Keenan <me@patrickkeenan.me>
2024-03-18 22:49:21 -0400

    end to end

--- app/tests/export/scene.tsx
+++ app/tests/export/scene.tsx
@@ -7,27 +7,7 @@ import {
   Image,
   Content,
 } from "@react-three/uikit";
-import { Button } from "@/app/components/button";
-import { Badge } from "@/app/components/badge";
-import UI from "../../import/ui";
 export default function Layout() {
   const [fullscreen, setFullscreen] = useState(true);
-  return (
-    <>
-      <Fullscreen>
-        <UI
-          onSwitch={(fullscreen) => {
-            console.log("fullscreen", fullscreen);
-            setFullscreen(fullscreen);
-          }}
-        />
-        <Button>
-          <Text>I am red.</Text>
-        </Button>
-        <Badge>
-          <Text>Badge</Text>
-        </Badge>
-      </Fullscreen>
-    </>
-  );
+  return <></>;
 }
